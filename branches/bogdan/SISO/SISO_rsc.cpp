e5ae36900cdfe60cd2a1de0aa5f72c20edcac7a0
cristeab <cristeab@85eea152-77c2-11de-8a01-315fa775be5f>
2010-01-01 17:33:20 +0000

    - deleted CC.cpp file - added install option in Makefile - modified SISO RSC module interface in order to output extrinsic info for coded and data bits - added hard output flag for SISO Viterbi decoder

--- branches/bogdan/SISO/SISO_rsc.cpp
+++ branches/bogdan/SISO/SISO_rsc.cpp
@@ -57,7 +57,7 @@ void SISO::gen_rsctrellis(void)
 	}
 }
 
-void SISO::rsc_logMAP(itpp::vec &extrinsic_parity, itpp::vec &extrinsic_data,
+void SISO::rsc_logMAP(itpp::vec &extrinsic_coded, itpp::vec &extrinsic_data,
 		const itpp::vec &intrinsic_coded, const itpp::vec &apriori_data)
 /*
  logMAP (SISO) decoder for RSC of rate 1/2
@@ -173,6 +173,7 @@ void SISO::rsc_logMAP(itpp::vec &extrinsic_parity, itpp::vec &extrinsic_data,
 
 	//updated LLR for information bits
 	extrinsic_data.set_size(N);
+	extrinsic_coded.set_size(2*N);
 #pragma omp parallel for private(n, k, sum0, sum1)
 	for (n=0;n<N;n++)
 	{
@@ -184,10 +185,10 @@ void SISO::rsc_logMAP(itpp::vec &extrinsic_parity, itpp::vec &extrinsic_data,
 			sum0 += exp(A0[k+n*rsctrellis.numStates]+B0[k+n*rsctrellis.numStates]);
 		}
 		extrinsic_data[n] = log(sum1/sum0)-apriori_data[n];//updated information must be independent of input LLR
+		extrinsic_coded[2*n] = log(sum1/sum0)-Lc1I[n];//this information is used in serial concatenations
 	}
 
 	//updated LLR for coded (parity) bits
-	extrinsic_parity.set_size(N);
 #pragma omp parallel for private(n, k, sum0, sum1)
 	for (n=0;n<N;n++)
 	{
@@ -206,7 +207,7 @@ void SISO::rsc_logMAP(itpp::vec &extrinsic_parity, itpp::vec &extrinsic_data,
 				sum1 += exp(A0[k+n*rsctrellis.numStates]+B0[k+n*rsctrellis.numStates]);
 			}
 		}
-		extrinsic_parity[n] = log(sum0/sum1)-Lc2I[n];//updated information must be independent of input LLR
+		extrinsic_coded[2*n+1] = log(sum0/sum1)-Lc2I[n];//updated information must be independent of input LLR
 	}
 
 	//destroy trellis
@@ -224,7 +225,7 @@ void SISO::rsc_logMAP(itpp::vec &extrinsic_parity, itpp::vec &extrinsic_data,
 	delete[] B1;
 }
 
-void SISO::rsc_maxlogMAP(itpp::vec &extrinsic_parity, itpp::vec &extrinsic_data,
+void SISO::rsc_maxlogMAP(itpp::vec &extrinsic_coded, itpp::vec &extrinsic_data,
 		const itpp::vec &intrinsic_coded, const itpp::vec &apriori_data)
 /*
  maxlogMAP (SISO) decoder for RSC of rate 1/2
@@ -335,6 +336,7 @@ void SISO::rsc_maxlogMAP(itpp::vec &extrinsic_parity, itpp::vec &extrinsic_data,
 
 	//updated LLR for information bits
 	extrinsic_data.set_size(N);
+	extrinsic_coded.set_size(2*N);
 #pragma omp parallel for private(n, k, sum0, sum1)
 	for (n=0;n<N;n++)
 	{
@@ -346,10 +348,10 @@ void SISO::rsc_maxlogMAP(itpp::vec &extrinsic_parity, itpp::vec &extrinsic_data,
 			sum0 = std::max(sum0, A0[k+n*rsctrellis.numStates]+B0[k+n*rsctrellis.numStates]);
 		}
 		extrinsic_data[n] = (sum1-sum0)-apriori_data[n];//updated information must be independent of input LLR
+		extrinsic_coded[2*n] = (sum1-sum0)-Lc1I[n];
 	}
 
 	//updated LLR for coded (parity) bits
-	extrinsic_parity.set_size(N);
 #pragma omp parallel for private(n, k, sum0, sum1)
 	for (n=0;n<N;n++)
 	{
@@ -368,7 +370,7 @@ void SISO::rsc_maxlogMAP(itpp::vec &extrinsic_parity, itpp::vec &extrinsic_data,
 				sum1 = std::max(sum1, A0[k+n*rsctrellis.numStates]+B0[k+n*rsctrellis.numStates]);
 			}
 		}
-		extrinsic_parity[n] = (sum0-sum1)-Lc2I[n];//updated information must be independent of input LLR
+		extrinsic_coded[2*n+1] = (sum0-sum1)-Lc2I[n];//updated information must be independent of input LLR
 	}
 	//destroy trellis
 	delete[] rsctrellis.prevStates;
@@ -454,94 +456,94 @@ void SISO::rsc_sova(itpp::vec &extrinsic_data, const itpp::vec &intrinsic_coded,
 	extrinsic_data.set_size(len);
 	for(i = 0; i < len; i++)
 	{
-	    //indices + precalculations
+		//indices + precalculations
 		Cur = i%(win_len+1);//curr trellis (cycl. buf) position
 		Nxt = (i+1)%(win_len+1);//next trellis (cycl. buf) position
 		buf = intrinsic_coded(i*nb_outputs,(i+1)*nb_outputs-1);//intrinsic_info portion to be processed
-	    llb = apriori_data(i);//SOVA: apriori_info portion to be processed
-	    metr.set_col(Nxt, -INFINITY*itpp::ones(nb_states));
+		llb = apriori_data(i);//SOVA: apriori_info portion to be processed
+		metr.set_col(Nxt, -INFINITY*itpp::ones(nb_states));
 
-	    //forward recursion
-	    for(s = 0; s<nb_states; s++)
-	    {
-	        for(j = 0; j<2; j++)
-	        {
-	            nxt = next_state(s,j);//state after transition
-	            bin = bin_out(j).get_row(s);//transition output (encoder)
-	            mtr = bin*buf+metr(s,Cur);//transition metric
-	            mtr += bin_inp(j)*llb;//SOVA
+		//forward recursion
+		for(s = 0; s<nb_states; s++)
+		{
+			for(j = 0; j<2; j++)
+			{
+				nxt = next_state(s,j);//state after transition
+				bin = bin_out(j).get_row(s);//transition output (encoder)
+				mtr = bin*buf+metr(s,Cur);//transition metric
+				mtr += bin_inp(j)*llb;//SOVA
 
-	            if(metr(nxt,Nxt) < mtr)
-	            {
-	                diff(nxt,Nxt) = mtr-metr(nxt,Nxt);//SOVA
-	                comp(nxt,Nxt) = surv(nxt,Nxt);//SOVA
-	                inpc(nxt,Nxt) = inpt(nxt,Nxt);//SOVA
+				if(metr(nxt,Nxt) < mtr)
+				{
+					diff(nxt,Nxt) = mtr-metr(nxt,Nxt);//SOVA
+					comp(nxt,Nxt) = surv(nxt,Nxt);//SOVA
+					inpc(nxt,Nxt) = inpt(nxt,Nxt);//SOVA
 
-	                metr(nxt,Nxt) = mtr;//store the metric
-	                surv(nxt,Nxt) = s;//store the survival state
-	                inpt(nxt,Nxt) = j;//store the survival input
-	            }
-	            else
-	            {
-	                dif = metr(nxt,Nxt)-mtr;
-	                if(dif <= diff(nxt,Nxt))
-	                {
-	                    diff(nxt,Nxt) = dif;//SOVA
-	                    comp(nxt,Nxt) = s;//SOVA
-	                    inpc(nxt,Nxt) = j;//SOVA
-	                }
-	            }
-	        }
-	    }
+					metr(nxt,Nxt) = mtr;//store the metric
+					surv(nxt,Nxt) = s;//store the survival state
+					inpt(nxt,Nxt) = j;//store the survival input
+				}
+				else
+				{
+					dif = metr(nxt,Nxt)-mtr;
+					if(dif <= diff(nxt,Nxt))
+					{
+						diff(nxt,Nxt) = dif;//SOVA
+						comp(nxt,Nxt) = s;//SOVA
+						inpc(nxt,Nxt) = j;//SOVA
+					}
+				}
+			}
+		}
 
-	    //trace backwards
-	    if(i < (win_len-1))
-	    {
-	        continue;
-	    }//proceed if the buffer has been filled
-	    mtr = itpp::max(metr.get_col(Nxt), sur);//find the initial state (max metric)
-	    b = i;//temporary bit index
-        for(j=0; j<win_len; j++)//indices in a 'cyclic buffer' operation
-        {
-        	cyclic_buffer(j) = (Nxt-j)%(win_len+1);
-            cyclic_buffer(j) = (cyclic_buffer(j)<0)?(cyclic_buffer(j)+win_len+1):cyclic_buffer(j);
-        }
+		//trace backwards
+		if(i < (win_len-1))
+		{
+			continue;
+		}//proceed if the buffer has been filled
+		mtr = itpp::max(metr.get_col(Nxt), sur);//find the initial state (max metric)
+		b = i;//temporary bit index
+		for(j=0; j<win_len; j++)//indices in a 'cyclic buffer' operation
+		{
+			cyclic_buffer(j) = (Nxt-j)%(win_len+1);
+			cyclic_buffer(j) = (cyclic_buffer(j)<0)?(cyclic_buffer(j)+win_len+1):cyclic_buffer(j);
+		}
 
-	    for(j=0; j<win_len; j++)//for all the bits in the buffer
-	    {
-	        inp = inpt(sur,cyclic_buffer(j));//current bit-decoder output (encoder input)
-	        extrinsic_data(b) = inp;//store the hard output
+		for(j=0; j<win_len; j++)//for all the bits in the buffer
+		{
+			inp = inpt(sur,cyclic_buffer(j));//current bit-decoder output (encoder input)
+			extrinsic_data(b) = inp;//store the hard output
 
-	        tmp = cyclic_buffer(j);
-	        cmp = comp(sur, tmp);//SOVA: competitor state (previous)
-	        inc = inpc(sur, tmp);//SOVA: competitor bit output
-	        dif = diff(sur, tmp);//SOVA: corresp. path metric difference
-	        srv = surv(sur, tmp);//SOVA: temporary survivor path state
+			tmp = cyclic_buffer(j);
+			cmp = comp(sur, tmp);//SOVA: competitor state (previous)
+			inc = inpc(sur, tmp);//SOVA: competitor bit output
+			dif = diff(sur, tmp);//SOVA: corresp. path metric difference
+			srv = surv(sur, tmp);//SOVA: temporary survivor path state
 
-	        for(s=j+1; s<=win_len; s++)//check all buffer bits srv and cmp paths
-	        {
-	        	if(inp != inc)
-	        	{
-	        		idx = b-((s-1)-j);//calculate index: [enc.k*(b-(k-1)+j-1)+1:enc.k*(b-(k-1)+j)]
-	        		sft(idx) = std::min(sft(idx), dif);//update LLRs for bits that are different
-	        	}
-	        	if(srv == cmp)
-	        	{
-	        		break;
-	        	}//stop if surv and comp merge (no need to continue)
-	            if(s == win_len)
-	            {
-	                break;
-	            }//stop if the end (otherwise: error)
-	            tmp = cyclic_buffer(s);
-	            inp = inpt(srv, tmp);//previous surv bit
-	            inc = inpt(cmp, tmp);//previous comp bit
-	            srv = surv(srv, tmp);//previous surv state
-	            cmp = surv(cmp, tmp);//previous comp state
-	        }
-	        sur = surv(sur, cyclic_buffer(j));//state for the previous surv bit
-	        b--;//update bit index
-	    }
+			for(s=j+1; s<=win_len; s++)//check all buffer bits srv and cmp paths
+			{
+				if(inp != inc)
+				{
+					idx = b-((s-1)-j);//calculate index: [enc.k*(b-(k-1)+j-1)+1:enc.k*(b-(k-1)+j)]
+					sft(idx) = std::min(sft(idx), dif);//update LLRs for bits that are different
+				}
+				if(srv == cmp)
+				{
+					break;
+				}//stop if surv and comp merge (no need to continue)
+				if(s == win_len)
+				{
+					break;
+				}//stop if the end (otherwise: error)
+				tmp = cyclic_buffer(s);
+				inp = inpt(srv, tmp);//previous surv bit
+				inc = inpt(cmp, tmp);//previous comp bit
+				srv = surv(srv, tmp);//previous surv state
+				cmp = surv(cmp, tmp);//previous comp state
+			}
+			sur = surv(sur, cyclic_buffer(j));//state for the previous surv bit
+			b--;//update bit index
+		}
 	}
 
 	// provide soft output with +/- sign:
@@ -556,8 +558,8 @@ void SISO::rsc_sova(itpp::vec &extrinsic_data, const itpp::vec &intrinsic_coded,
 	delete[] rsctrellis.fm;
 }
 
-void SISO::rsc_viterbi(itpp::vec &extrinsic_data, const itpp::vec &intrinsic_coded,
-		const itpp::vec &apriori_data, const int &win_len)
+void SISO::rsc_viterbi(itpp::vec &extrinsic_coded, itpp::vec &extrinsic_data,
+		const itpp::vec &intrinsic_coded, const itpp::vec &apriori_data, const int &win_len)
 /* Soft Input Soft Output module based on Viterbi algorithm
  * Output: extrinsic_data - extrinsic information of data bits
  * Inputs: intrinsic_coded - intrinsic information of data bits
@@ -599,77 +601,96 @@ void SISO::rsc_viterbi(itpp::vec &extrinsic_data, const itpp::vec &intrinsic_cod
 	metr(0,0) = 0;//initial state => (0,0)
 	itpp::mat surv(nb_states,win_len+1);//survivor state buffer
 	surv.zeros();
-	itpp::mat inpt(nb_states,win_len+1);//survivor input buffer (dec. output)
+	itpp::mat inpt(nb_states,win_len+1);//survivor input bits buffer (dec. output)
 	inpt.zeros();
+	itpp::mat parity(nb_states,win_len+1);//survivor parity bits buffer (dec. output)
+	parity.zeros();
 
 	//decode all the bits
 	int Cur,Nxt,nxt,sur,b;
 	itpp::vec buf(nb_outputs);
-	double llb,mtr,inp;
+	double llb,mtr;
 	itpp::vec bin(nb_outputs);
 	itpp::ivec cyclic_buffer(win_len);
-	itpp::bvec hard_output(len);
+	extrinsic_coded.set_size(2*len);//initialize memory for output
+	extrinsic_data.set_size(len);
 	for(i = 0; i < len; i++)
 	{
-	    //indices + precalculations
+		//indices + precalculations
 		Cur = i%(win_len+1);//curr trellis (cycl. buf) position
 		Nxt = (i+1)%(win_len+1);//next trellis (cycl. buf) position
 		buf = intrinsic_coded(i*nb_outputs,(i+1)*nb_outputs-1);//intrinsic_info portion to be processed
-	    llb = apriori_data(i);//SOVA: apriori_info portion to be processed
-	    metr.set_col(Nxt, -INFINITY*itpp::ones(nb_states));
+		llb = apriori_data(i);//SOVA: apriori_info portion to be processed
+		metr.set_col(Nxt, -INFINITY*itpp::ones(nb_states));
 
-	    //forward recursion
-	    for(s = 0; s<nb_states; s++)
-	    {
-	        for(j = 0; j<2; j++)
-	        {
-	            nxt = next_state(s,j);//state after transition
-	            bin = bin_out(j).get_row(s);//transition output (encoder)
-	            mtr = bin*buf+metr(s,Cur);//transition metric
-	            mtr += bin_inp(j)*llb;//add a priori info contribution
+		//forward recursion
+		for(s = 0; s<nb_states; s++)
+		{
+			for(j = 0; j<2; j++)
+			{
+				nxt = next_state(s,j);//state after transition
+				bin = bin_out(j).get_row(s);//transition output (encoder)
+				mtr = bin*buf+metr(s,Cur);//transition metric
+				mtr += bin_inp(j)*llb;//add a priori info contribution
 
-	            if(metr(nxt,Nxt) < mtr)
-	            {
-	                metr(nxt,Nxt) = mtr;//store the metric
-	                surv(nxt,Nxt) = s;//store the survival state
-	                inpt(nxt,Nxt) = j;//store the survival input
-	            }
-	        }
-	    }
+				if(metr(nxt,Nxt) < mtr)
+				{
+					metr(nxt,Nxt) = mtr;//store the metric
+					surv(nxt,Nxt) = s;//store the survival state
+					inpt(nxt,Nxt) = bin(0);//j;//store the survival input
+					parity(nxt,Nxt) = bin(1);//store survival parity bit
+				}
+			}
+		}
 
-	    //trace backwards
-	    if(i < (win_len-1))
-	    {
-	        continue;
-	    }//proceed if the buffer has been filled
-	    mtr = itpp::max(metr.get_col(Nxt), sur);//find the initial state (max metric)
-	    b = i;//temporary bit index
-        for(j=0; j<win_len; j++)//indices in a 'cyclic buffer' operation
-        {
-        	cyclic_buffer(j) = (Nxt-j)%(win_len+1);
-            cyclic_buffer(j) = (cyclic_buffer(j)<0)?(cyclic_buffer(j)+win_len+1):cyclic_buffer(j);
-        }
+		//trace backwards
+		if(i < (win_len-1))
+		{
+			continue;
+		}//proceed if the buffer has been filled
+		mtr = itpp::max(metr.get_col(Nxt), sur);//find the initial state (max metric)
+		b = i;//temporary bit index
+		for(j=0; j<win_len; j++)//indices in a 'cyclic buffer' operation
+		{
+			cyclic_buffer(j) = (Nxt-j)%(win_len+1);
+			cyclic_buffer(j) = (cyclic_buffer(j)<0)?(cyclic_buffer(j)+win_len+1):cyclic_buffer(j);
+		}
 
-	    for(j=0; j<win_len; j++)//for all the bits in the buffer
-	    {
-	        inp = inpt(sur,cyclic_buffer(j));//current bit-decoder output (encoder input)
-	        hard_output(b) = (inp==1.0);//store the hard output
-	        sur = surv(sur, cyclic_buffer(j));//state for the previous surv bit
-	        b--;//update bit index
-	    }
+		for(j=0; j<win_len; j++)//for all the bits in the buffer
+		{
+			extrinsic_data(b) = inpt(sur,cyclic_buffer(j));//current bit-decoder output (encoder input)
+			extrinsic_coded(2*b) = extrinsic_data(b);
+			extrinsic_coded(2*b+1) = parity(sur,cyclic_buffer(j));
+			sur = surv(sur, cyclic_buffer(j));//state for the previous surv bit
+			b--;//update bit index
+		}
 	}
 
-	// provide soft output (extrinsic information) for informational bits
+	//free trellis memory
+	delete[] rsctrellis.prevStates;
+	delete[] rsctrellis.nextStates;
+	delete[] rsctrellis.PARout;
+	delete[] rsctrellis.fm;
+
+	//output only the hard output if the flag is true
+	if(Viterbi_hard_output_flag==true)
+	{
+		return;
+	}
+
+	// provide soft output (extrinsic information) for data bits
 	//compute flipped and non-flipped bits positions
 	itpp::bvec tmp(len);
 	for(i=0; i<len; i++)
 	{
-		tmp(i) = ((apriori_data(i)+intrinsic_coded(2*i))>=0)^hard_output(i);
+		tmp(i) = ((apriori_data(i)+intrinsic_coded(2*i))>=0)^(extrinsic_data(i)==1.0);
 	}
-	itpp::ivec idx_matching = itpp::find(tmp==itpp::bin(0));
-	itpp::ivec idx_nonmatching = itpp::find(tmp==itpp::bin(1));
+	itpp::ivec *ptr_idx_matching = new itpp::ivec;
+	itpp::ivec *ptr_idx_nonmatching = new itpp::ivec;
+	*ptr_idx_matching =	itpp::find(tmp==itpp::bin(0));
+	*ptr_idx_nonmatching = itpp::find(tmp==itpp::bin(1));
 	//Estimated Bit Error Rate
-	int idx_nonmatching_len = idx_nonmatching.length();
+	int idx_nonmatching_len = ptr_idx_nonmatching->length();
 	double error_rate = double(idx_nonmatching_len)/double(len);
 	//Logarithm of Likelihood Ratio
 	double LLR;
@@ -683,25 +704,59 @@ void SISO::rsc_viterbi(itpp::vec &extrinsic_data, const itpp::vec &intrinsic_cod
 	{
 		LLR = std::log((1.0-error_rate)/error_rate);
 	}
-	extrinsic_data.set_size(len);
-	for(i=0; i<idx_matching.length(); i++)
+	for(i=0; i<ptr_idx_matching->length(); i++)
 	{
-		extrinsic_data(idx_matching(i)) = Viterbi_scaling_factor[0]*
-				(2.0*double(hard_output(idx_matching(i)))-1.0)*LLR;
+		extrinsic_data(ptr_idx_matching->get(i)) = Viterbi_scaling_factor[0]*
+				(2.0*extrinsic_data(ptr_idx_matching->get(i))-1.0)*LLR;
 	}
 	for(i=0; i<idx_nonmatching_len; i++)
 	{
-		extrinsic_data(idx_nonmatching(i)) = Viterbi_scaling_factor[1]*
-				(2.0*double(hard_output(idx_nonmatching(i)))-1.0)*LLR;
+		extrinsic_data(ptr_idx_nonmatching->get(i)) = Viterbi_scaling_factor[1]*
+				(2.0*extrinsic_data(ptr_idx_nonmatching->get(i))-1.0)*LLR;
 	}
 	//extrinsic information
 	extrinsic_data -= apriori_data;
 
-	//free trellis memory
-	delete[] rsctrellis.prevStates;
-	delete[] rsctrellis.nextStates;
-	delete[] rsctrellis.PARout;
-	delete[] rsctrellis.fm;
+	//provide soft output for coded bits
+	tmp.set_length(2*len);
+	for(i=0; i<(2*len); i++)
+	{
+		tmp(i) = (intrinsic_coded(i)>=0)^(extrinsic_coded(i)==1.0);
+	}
+	delete ptr_idx_matching;//free old memory
+	delete ptr_idx_nonmatching;
+	ptr_idx_matching = new itpp::ivec;//allocate memory for new vectors
+	ptr_idx_nonmatching = new itpp::ivec;
+	*ptr_idx_matching = itpp::find(tmp==itpp::bin(0));
+	*ptr_idx_nonmatching = itpp::find(tmp==itpp::bin(1));
+	//Estimated Bit Error Rate
+	idx_nonmatching_len = ptr_idx_nonmatching->length();
+	error_rate = double(idx_nonmatching_len)/double(2*len);
+	//Logarithm of Likelihood Ratio
+	if(error_rate==0.0)
+	{
+		LLR = std::log(double(2*len));
+	} else if(error_rate==1.0)
+	{
+		LLR = -std::log(double(2*len));
+	} else
+	{
+		LLR = std::log((1.0-error_rate)/error_rate);
+	}
+	for(i=0; i<ptr_idx_matching->length(); i++)
+	{
+		extrinsic_coded(ptr_idx_matching->get(i)) = Viterbi_scaling_factor[0]*
+				(2.0*extrinsic_coded(ptr_idx_matching->get(i))-1.0)*LLR;
+	}
+	for(i=0; i<idx_nonmatching_len; i++)
+	{
+		extrinsic_coded(ptr_idx_nonmatching->get(i)) = Viterbi_scaling_factor[1]*
+				(2.0*extrinsic_coded(ptr_idx_nonmatching->get(i))-1.0)*LLR;
+	}
+	delete ptr_idx_matching;
+	delete ptr_idx_nonmatching;
+	//extrinsic information
+	extrinsic_coded -= intrinsic_coded;
 }
 
 }//namespace tr
